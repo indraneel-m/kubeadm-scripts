d77dc0c6d8b9928b7a591a18933aed08bf11ab26
Dennis Maisenbacher <dennis.maisenbacher@wdc.com>
2023-02-10 12:58:37 +0100

    WIP: Fix calico deployment
    Signed-off-by: Dennis Maisenbacher <dennis.maisenbacher@wdc.com>

--- scripts/master-setup.sh
+++ scripts/master-setup.sh
@@ -7,8 +7,10 @@ mkdir -p $HOME/.kube
 sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
 sudo chown $(id -u):$(id -g) $HOME/.kube/config
 
-curl https://docs.projectcalico.org/manifests/calico.yaml -O
+curl https://raw.githubusercontent.com/projectcalico/calico/v3.25.0/manifests/calico.yaml -O
 kubectl apply -f calico.yaml
+#https://github.com/openebs/mayastor/issues/1317#issuecomment-1425427761
+#kubectl patch felixconfigurations default --patch '{"spec":{"featureDetectOverride":"ChecksumOffloadBroken=true"}}' --type=merge
 
 curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
 chmod 700 get_helm.sh
